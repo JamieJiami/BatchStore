761492430faff9d81221e6f42c1b729288693678
JamieJiami <75553178+JamieJiami@users.noreply.github.com>
2022-04-12 17:30:56 +0800

    Update Main.bat

--- Main.bat
+++ Main.bat
@@ -1,3 +1,11 @@
 @echo off
-title BatchStore v1.0
-pause
+title BatchGamesStore v1.0
+echo -----Select Games-----
+echo.
+echo 1. Overcooked Gourment Edition
+echo 2. Overcooked 2
+echo 3. Overcooked All You Can Eat with All DLC
+echo 4. Plague Inc Evolved
+echo.
+echo More Coming Soon!
+
